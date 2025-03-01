d7c09691900fdd02ad4a08632ba615e4dd8688c3
stfen <Stefoo123@outlook.com>
2025-03-01 01:01:49 +0100

    Added generating RSA keys and saving them to selected drive

--- drive_manager/drive_manager.py
+++ drive_manager/drive_manager.py
@@ -12,12 +12,14 @@ class DriveManager:
         """
         logger.info("Drive manager's instance created")
         self.drive_list = []
+        self.selected_drive = None
 
     def refresh(self) -> list[str]:
         """
         Returns a list of USB drives
         """
         self.drive_list = [disk.device for disk in psutil.disk_partitions() if "removable" in disk.opts]
+        self.drive_list = [disk.device for disk in psutil.disk_partitions()]
         logger.info("Detected devices: %s", self.drive_list)
 
     def read_files(self, path: str) -> list[str]:
@@ -47,4 +49,44 @@ class DriveManager:
         else:
             logger.info("Files in %s: %s", path, files)
             return files
+
+    def save_to_drive(self, data: bytes, destination_name: str) -> bool:
+        """
+        Saves binary data to a file on the selected drive.
+
+        Args:
+            data (bytes): Binary data to be saved on the USB drive.
+            destination_name (str): Name of the file on the selected drive.
+
+        Returns:
+            bool: True if the data is successfully saved, False otherwise.
+
+        """
+        if not self.selected_drive:
+            logger.error("No drive selected. Set 'self.selected_drive' before saving.")
+            return False
+
+        if not Path(self.selected_drive).exists():
+            logger.error("Selected drive does not exist: %s", self.selected_drive)
+            return False
+
+        destination_path = Path(self.selected_drive) / destination_name
+
+        success = False
+        try:
+            with destination_path.open("wb") as dest:
+                dest.write(data)
+
+            logger.info("File successfully saved to: %s", destination_path)
+            success = True
+
+        except PermissionError:
+            logger.exception("Permission denied when writing to: %s", destination_path)
+        except OSError:
+            logger.exception("I/O error occurred while writing to: %s", destination_path)
+        except Exception:
+            logger.exception("Unexpected error occurred while saving to: %s", destination_path)
+
+        return success
+
 
